afb6b4102783b2c441730432fae974146f994965
Mona <mona@Monas-MacBook-Pro.local>
2020-10-14 19:42:16 -0400

    Brought React to Personal Budget

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer className="bottom">
+        <div className="center">
+            All rights reserved &copy; Divya Pachika
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
